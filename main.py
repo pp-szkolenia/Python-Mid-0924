4a153fa883339e532251cfa26b6a74a3546afb8a
Patryk Palej <patrykpalej@gmail.com>
2024-09-11 10:57:14 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, Body
+
+
+app = FastAPI()
+
+tasks_data = [
+    {"description": "Learn FastAPI", "priority": 3, "is_complete": True},
+    {"description": "Do exercises", "priority": 2, "is_complete": False}
+]
+
+users_data = [
+    {"username": "Andrzej", "password": "qwerty123", "is_admin": True},
+    {"username": "Andżela", "password": "hasło1!", "is_admin": False},
+]
+
+
+@app.get("/")
+def root():
+    return "hello world"
+
+
+@app.get("/tasks")
+def get_tasks():
+    return {"result": tasks_data}
+
+
+@app.post("/tasks")
+def create_task(body: dict = Body(...)):
+    new_task = body
+    tasks_data.append(new_task)
+
+    return {"message": "New task added",
+            "details": new_task}
+
+
+@app.get("/users/")
+def get_users():
+    return {"result": users_data}

3de448a876961e9088aa34d392e031f5abc90ce3
Patryk Palej <patrykpalej@gmail.com>
2024-09-11 11:30:45 +0200

    Walidacja modelem BaseModel

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 from fastapi import FastAPI, Body
+from pydantic import BaseModel, PositiveInt, model_validator
 
 
 app = FastAPI()
 
+
+class TaskBody(BaseModel):
+    """
+    TaskBody jest modelem request body używanym w endpoincie POST /tasks
+    Waliduje on poprawność użytego w requeście body
+    """
+    description: str
+    priority: PositiveInt  # int | None = None  # dopuszczamy też None i None jest domyślne
+    is_complete: bool = False  # domyślnie jest False
+
+    @model_validator(mode="after")
+    def validate(self):
+        """
+        mode - after/before mówi o tym czy walidator odpala się przed walidacją
+                typów czy po
+        Tutaj odbywa się bardziej zaawansowana walidacja modelu danych
+        Przyjmujemy i zwracamy self
+        """
+        # if len(self.description) > self.priority:
+        #     raise AssertionError("Description must be less than priority")
+        return self
+
+
 tasks_data = [
-    {"description": "Learn FastAPI", "priority": 3, "is_complete": True},
-    {"description": "Do exercises", "priority": 2, "is_complete": False}
+    {"id": 1, "description": "Learn FastAPI", "priority": 3, "is_complete": True},
+    {"id": 2, "description": "Do exercises", "priority": 2, "is_complete": False}
 ]
 
 users_data = [
-    {"username": "Andrzej", "password": "qwerty123", "is_admin": True},
-    {"username": "Andżela", "password": "hasło1!", "is_admin": False},
+    {"id": 1, "username": "Andrzej", "password": "qwerty123", "is_admin": True},
+    {"id": 2, "username": "Andżela", "password": "hasło1!", "is_admin": False},
 ]
 
 
@@ -25,8 +49,11 @@ def get_tasks():
 
 
 @app.post("/tasks")
-def create_task(body: dict = Body(...)):
-    new_task = body
+def create_task(body: TaskBody):
+    new_task = body.model_dump()
+    task_id = len(tasks_data) + 1
+    new_task["id"] = task_id
+
     tasks_data.append(new_task)
 
     return {"message": "New task added",
@@ -36,3 +63,11 @@ def create_task(body: dict = Body(...)):
 @app.get("/users/")
 def get_users():
     return {"result": users_data}
+
+
+@app.post("/users/")
+def create_user(body: dict = Body(...)):
+    new_user = body
+    users_data.append(new_user)
+
+    return {"message": "New user added", "details": new_user}
